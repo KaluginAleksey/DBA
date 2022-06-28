4c7619c58416aa1f08b46e4bd55ad524b5239063
KaluginAleksey <mr.kamc@yandex.ru>
2022-06-28 14:06:30 +0300

    добавил запросы в postgres.sql

--- mysql.sql
+++ mysql.sql
@@ -13,9 +13,9 @@ CREATE TABLE `shop`
 
 
 -- Выбирают 10 самых новых товаров
--- 1 row retrieved starting from 1 in 143 ms (execution: 61 ms, fetching: 82 ms)
--- после добавления индекса
--- 1 row retrieved starting from 1 in 86 ms (execution: 17 ms, fetching: 69 ms)
+-- execution: 61 ms, fetching: 82 ms
+-- execution: 34 ms, fetching: 70 ms после добавления индекса `date_idx`
+-- Backward index scan
 EXPLAIN
 SELECT *
 FROM `shop`
@@ -24,9 +24,8 @@ ORDER BY `date`
 LIMIT 10;
 
 -- Выбирают 10 самых дешевых товаров
--- 1 row retrieved starting from 1 in 116 ms (execution: 35 ms, fetching: 81 ms)
--- после добавления индекса
--- 1 row retrieved starting from 1 in 86 ms (execution: 16 ms, fetching: 70 ms)
+-- execution: 35 ms, fetching: 81 ms
+
 EXPLAIN
 SELECT *
 FROM `shop`
@@ -34,9 +33,8 @@ ORDER BY `price`
 LIMIT 10;
 
 -- Выбирают 10 товаров, цена на которых была максимально снижена (в абсолютном или относительном смысле)
--- 1 row retrieved starting from 1 in 82 ms (execution: 25 ms, fetching: 57 ms)
--- после добавления индекса
--- 1 row retrieved starting from 1 in 73 ms (execution: 22 ms, fetching: 51 ms)
+-- execution: 25 ms, fetching: 57 ms
+
 EXPLAIN SELECT *
 FROM `shop`
 WHERE `old_price` IS NOT NULL
@@ -44,12 +42,13 @@ ORDER BY (`price` - `old_price`)
 LIMIT 10;
 
 -- Выбирают те товары, чей артикул начинается с символов "TEST" (такие товары должны быть в таблице!), используя оператор LIKE
--- 4 rows retrieved starting from 1 in 61 ms (execution: 18 ms, fetching: 43 ms)
+-- execution: 18 ms, fetching: 43 ms
 -- после добавления индекса
--- 1 row retrieved starting from 1 in 74 ms (execution: 20 ms, fetching: 54 ms)
+-- execution: 20 ms, fetching: 54 ms
 -- отображается добавленный индекс в поле possible_keys. Using index condition
 EXPLAIN SELECT *
 FROM `shop`
 WHERE `num` LIKE 'TEST%';
 
 CREATE INDEX `num_idx` ON `shop` (`num`);
+CREATE INDEX `date_idx` ON `shop` (`date`);
--- postgres.sql
+++ postgres.sql
@@ -10,3 +10,45 @@ CREATE TABLE "shop"
     "qt" BIGINT,
     PRIMARY KEY ("id")
 );
+
+-- Выбирают 10 самых новых товаров
+-- execution: 19 ms, fetching: 67 ms Sort Key: date DESC
+-- после добавления индекса "date_idx"
+-- execution: 7 ms, fetching: 46 ms Index Scan Backward using date_idx on shop
+EXPLAIN
+SELECT *
+FROM "shop"
+    ORDER BY "date"
+    DESC
+    LIMIT 10;
+
+-- Выбирают 10 самых дешевых товаров
+-- execution: 19 ms, fetching: 79 ms Sort Key: price
+-- после добавления индекса
+--
+EXPLAIN
+SELECT *
+FROM "shop"
+    ORDER BY "price"
+    LIMIT 10;
+
+-- Выбирают 10 товаров, цена на которых была максимально снижена (в абсолютном или относительном смысле)
+-- execution: 15 ms, fetching: 71 ms
+-- после добавления индекса
+--
+EXPLAIN SELECT *
+        FROM "shop"
+    WHERE "old_price" IS NOT NULL
+    ORDER BY ("price" - "old_price")
+    LIMIT 10;
+
+-- Выбирают те товары, чей артикул начинается с символов "TEST" (такие товары должны быть в таблице!), используя оператор LIKE
+-- execution: 12 ms, fetching: 49 ms
+-- после добавления индекса
+-- execution: 9 ms, fetching: 42 ms
+EXPLAIN SELECT *
+        FROM "shop"
+    WHERE "num" LIKE 'TEST%';
+
+CREATE INDEX "num_idx" ON "shop" ("num");
+CREATE INDEX "date_idx" ON "shop" ("date");
