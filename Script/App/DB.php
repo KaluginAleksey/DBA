f08f127329bb79e8ed0bb07fd3d042aa659d44b0
KaluginAleksey <mr.kamc@yandex.ru>
2022-07-13 18:36:42 +0300

    +

--- Script/App/DB.php
+++ Script/App/DB.php
@@ -1,24 +1,40 @@
 <?php
 
-namespace App;
+use App\DB;
 
+require_once __DIR__ . '/App/DB.php';
 
-class DB
-{
-    protected \PDO $dbh;
+function randomDate($start, $end) {
+    $min = strtotime($start);
+    $max = strtotime($end);
 
-    public function __construct($base)
-    {
-        $config = include __DIR__ . '/../Data/config.php';
-        $this->dbh = new \PDO($config['db'][$base]['dsn'], $config['db'][$base]['user'], $config['db'][$base]['password']);
-    }
+    $val = rand($min, $max);
 
+    return date('Y-m-d', $val);
+}
 
-    public function execute(string $sql, array $data = []): bool
-    {
-        $sth = $this->dbh->prepare($sql);
-        return $sth ->execute($data);
+$dbMysql = new DB('mysql');
 
-    }
+$i = 0;
 
+while ($i < 1000) {
+
+    $data = [
+        ':num' => rand(1000000000, 9999999999),
+        ':name' => 'товар' . $i,
+        ':price' => rand(10, 2000),
+        ':old_price' => rand(10, 2000),
+        ':img' => '/img/pic' . $i . '.jpg',
+        ':date' => randomDate('2014-10-01', '2022-06-27'),
+        ':qt' => rand(0, 3000),
+        ':id_category' => rand(1, 3),
+        ':id_brand' => rand(1, 7)
+    ];
+
+    $dbMysql->execute("INSERT INTO shop
+    (num, name, price, old_price, img, date, qt, id_category, id_brand)
+VALUES (:num, :name, :price, :old_price, :img, :date, :qt, :id_category, :id_brand)"
+        , $data);
+
+    $i++;
 }
