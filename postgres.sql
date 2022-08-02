f9b167ff1f15b5abe955ce991f1dbff64172e0cd
KaluginAleksey <mr.kamc@yandex.ru>
2022-08-02 20:03:20 +0300

    +

--- postgres.sql
+++ postgres.sql
@@ -1,14 +1,14 @@
 CREATE TABLE "shop"
 (
     "id"          SERIAL PRIMARY KEY,
-    "num"         CHAR(10)      NOT NULL UNIQUE,
+    "num"         CHAR(10)      NOT NULL UNIQUE CHECK ( "num"  SIMILAR TO '\A[A-z0-9_]{5,10}\Z' ),
     "name"        VARCHAR(150)  NOT NULL,
-    "price"       DECIMAL(8, 2) NOT NULL,
+    "price"       DECIMAL(8, 2) NOT NULL CHECK ( "price" > 0 ),
     "old_price"   DECIMAL(8, 2) DEFAULT NULL,
-    "img"         VARCHAR(200),
+    "img"         VARCHAR(200) CHECK ( "img" SIMILAR TO '%.jpg|%.jpeg|%.png\Z'),
     "date"        DATE,
-    "qt"          BIGINT,
-    "id_category" BIGINT,
+    "qt"          BIGINT CHECK ( "qt" >= 0 ),
+    "id_category" BIGINT CHECK ( "id_category" > 0 AND "id_category" < 4),
     "id_brand"    BIGINT
 );
 
@@ -25,13 +25,13 @@ CREATE TABLE "brand"
 );
 
 INSERT INTO "category"
-    ("cat")
+("cat")
 VALUES ('Еда'),
        ('Посуда'),
        ('Обувь');
 
 INSERT INTO "brand"
-    ("name")
+("name")
 VALUES ('Nestle'),
        ('Данон'),
        ('Красный Октябрь'),
@@ -54,71 +54,21 @@ ALTER TABLE "shop"
         ON DELETE RESTRICT
         ON UPDATE CASCADE;
 
--- Запрос, который выберет категории и среднюю цену товаров в каждой категории, при условии,
--- что эта средняя цена менее 1000 рублей (выбираем "бюджетные" категории товаров)
+-- Перепроектируйте таблицу товаров, используя поле categories bigint[]
 
-SELECT cat, AVG(price)
+ALTER TABLE "shop"
+    ADD "categories" BIGINT[];
+
+-- Выбирающий все товары из заданной категории
+
+SELECT * FROM shop
+WHERE 3=ANY(shop.categories);
+
+-- Выбирающий все категории и количество товаров в каждой из них
+
+SELECT cat AS categories, count(*) AS number_of_goods
 FROM shop
-         INNER JOIN category AS c ON shop.id_category = c.id
-GROUP BY cat
-HAVING AVG(price) < 1000;
+         INNER JOIN category on category.id = ANY(shop.categories)
+GROUP BY category.id;
 
--- Улучшите предыдущий запрос таким образом, чтобы в расчет средней цены включались только товары, имеющиеся на складе.
-
-SELECT cat, AVG(price)
-FROM shop
-         INNER JOIN category AS c ON shop.id_category = c.id
-WHERE qt > 0
-GROUP BY cat
-HAVING AVG(price) < 1000;
-
--- Добавьте к таблице брендов класс бренда (A, B, C). Например, A - Apple, B - Samsung, C - Xiaomi.
-
-ALTER TABLE "brand"
-    ADD COLUMN "class"
-        CHAR(1);
-
--- Напишите запрос, который для каждой категории и класса брендов, представленных в категории выберет среднюю цену товаров.
-
-SELECT cat, class, AVG(price)
-FROM shop
-         INNER JOIN brand AS b ON shop.id_brand = b.id
-         INNER JOIN category AS c ON id_category = c.id
-GROUP BY cat, class
-ORDER BY cat;
-
--- Добавьте к своей базе данных таблицу заказов. Простейший вариант - номер заказа, дата и время, ID товара.
-
-CREATE TABLE "orders"
-(
-    "id"      SERIAL PRIMARY KEY,
-    "num"     BIGINT                              NOT NULL,
-    "date"    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    "product" BIGINT                              NOT NULL
-);
-
--- Напишите запрос, который выведет таблицу с полями "дата", "число заказов за дату", "сумма заказов за дату".
-
-SELECT DATE(o.date) AS date, COUNT(o.id) AS amount, SUM(price) AS sum
-FROM orders AS o
-         INNER JOIN shop AS s ON o.product = s.id
-GROUP BY DATE(o.date);
-
--- Улучшите этот запрос, введя группировку по признаку "дешевый товар", "средняя цена", "дорогой товар".
--- Критерии отнесения товара к той или иной группе определите самостоятельно. В итоге должно получиться "дата",
--- "группа по цене", "число заказов", "сумма заказов"
-
-SELECT DATE(o.date) AS date,
-       COUNT(o.id)  AS amount,
-       SUM(price)   AS sum,
-       (CASE
-            WHEN price < 500 THEN 'Дешевый товар'
-            WHEN price >= 500 AND price < 1000 THEN 'Средняя цена'
-            WHEN price >= 1000 THEN 'Дорогой товар'
-           END)     AS feature
-FROM orders AS o
-         INNER JOIN shop s ON o.product = s.id
-GROUP BY DATE(o.date), feature
-ORDER BY DATE(o.date) DESC;
-
-
+-- Добавляющий определенный товар в определенную категорию (вам придется найти нужную функцию для работы с массивами)
